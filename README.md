b49f1c472bc5ca2452f5c61697df570220ba5261
kausare <kausarprogrammer@gmail.com>
2024-03-07 12:25:30 +0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # CodingNepal
+# webdevelopment
---
+++ js/main.js
@@ -0,0 +1,59 @@
+"use strict";
+
+
+/*----  ---*/
+
+
+var friendsName = "Md_Kausar_Ali";
+console.log(friendsName);
+
+// var friendsName = `Md_Kausar_Ali`;
+// console.log(typeof friendsName);
+
+var strType = 'Kausar';
+console.log(strType);
+
+var strType = 'Kausar';
+console.log(typeof strType);
+
+
+var numType = 1917828394;
+console.log(numType);
+
+var numType = 1917828394;
+console.log(typeof numType);
+
+
+var BoolType = false;
+console.log(typeof BoolType);
+
+
+var amiConvert = "200";
+amiConvert = parseInt(amiConvert);
+console.log(typeof amiConvert)
+
+
+var rochona = " is simply dummy text of the printing and typesetting industry. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.";
+console.log(rochona.toUpperCase());
+
+
+var rochonas = "IS SIMPLY DUMMY TEXT OF THE PRINTING AND TYPESETTING INDUSTRY. IT WAS POPULARISED IN THE 1960S WITH THE RELEASE OF LETRASET SHEETS CONTAINING LOREM IPSUM PASSAGES, AND MORE RECENTLY WITH DESKTOP PUBLISHING SOFTWARE LIKE ALDUS PAGEMAKER INCLUDING VERSIONS OF LOREM IPSUM.";
+console.log(rochonas.toLowerCase());
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+console.log("My Name Is Md Kausar Ali")
